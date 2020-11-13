2acb5b0867a937b50f6c38683e465dd67d8b1319
Marta Kowalczyk <azraindil@gmail.com>
2020-11-13 23:58:51 +0100

    pictures display

--- app/src/main/java/com/example/moodupteamtesttask/FirstFragment.java
+++ app/src/main/java/com/example/moodupteamtesttask/FirstFragment.java
@@ -2,9 +2,12 @@ package com.example.moodupteamtesttask;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,11 +18,16 @@ import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class FirstFragment extends Fragment {
-    private static final String EMAIL = "email";
-    CallbackManager callbackManager = CallbackManager.Factory.create();
-    LoginButton loginButton;
+    FloatingActionButton fab;
+    FloatingActionButton fab1;
+    FloatingActionButton fab2;
+    TextView txtRecipe, txtFacebook;
+    View layout1, layout2;
+    ImageView imgShadow;
+    boolean isFABOpen;
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
@@ -27,33 +35,58 @@ public class FirstFragment extends Fragment {
     ) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false);
-
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-       /* loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        fab = view.findViewById(R.id.fab);
+        fab1 = view.findViewById(R.id.fab1);
+        fab2 = view.findViewById(R.id.fab2);
+        imgShadow = view.findViewById(R.id.imgShadow);
+        txtFacebook= view.findViewById(R.id.txtFacebook);
+        txtRecipe= view.findViewById(R.id.txtRecipe);
+
+        imgShadow.setVisibility(View.INVISIBLE);
+        txtFacebook.setVisibility(View.INVISIBLE);
+        txtRecipe.setVisibility(View.INVISIBLE);
+
+        layout1 = view.findViewById(R.id.layout1);
+        layout2 = view.findViewById(R.id.layout2);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onSuccess(LoginResult loginResult) {
-                // App code
+            public void onClick(View view) {
+
+                if(!isFABOpen){
+                    showFABMenu();
+                }else{
+                    closeFABMenu();
+                }
+            }
+        });
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
-
-            @Override
-            public void onCancel() {
-                // App code
-            }
-
-            @Override
-            public void onError(FacebookException exception) {
-                // App code
-            }
         });
     }
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-        super.onActivityResult(requestCode, resultCode, data);*/
+    private void showFABMenu(){
+        isFABOpen=true;
+        fab.setRotation(45);
+        imgShadow.setVisibility(View.VISIBLE);
+        layout2.animate().translationY(-getResources().getDimension(R.dimen.standard_60));
+        layout1.animate().translationY(-getResources().getDimension(R.dimen.standard_120));
+        txtFacebook.setVisibility(View.VISIBLE);
+        txtRecipe.setVisibility(View.VISIBLE);
+    }
+    private void closeFABMenu(){
+        isFABOpen=false;
+        fab.setRotation(90);
+        imgShadow.setVisibility(View.INVISIBLE);
+        layout1.animate().translationY(0);
+        layout2.animate().translationY(0);
+        txtFacebook.setVisibility(View.INVISIBLE);
+        txtRecipe.setVisibility(View.INVISIBLE);
     }
 }
---
+++ app/src/main/java/com/example/moodupteamtesttask/RecipeInfo.java
@@ -0,0 +1,84 @@
+package com.example.moodupteamtesttask;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
+
+public class RecipeInfo extends AsyncTask<Void,Void,Void> {
+    String data = "";
+    String line = "";
+    String fullData;
+    String ingredients="", preparing="";
+    String img1, img2;
+    Bitmap bmp1, bmp2;
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            URL url = new URL("https://moodup.team/test/info.php");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            while(line != null){
+                line = bufferedReader.readLine();
+                data = data + line;
+            }
+            JSONObject json = new JSONObject(data);
+
+            JSONArray ingredientsArray = json.getJSONArray("ingredients");
+
+            for(int i=0; i< ingredientsArray.length();i++){
+
+                ingredients += "-" + ingredientsArray.getString(i) + "\n";
+            }
+
+            JSONArray preparingArray = json.getJSONArray("preparing");
+
+            for(int i=0; i< preparingArray.length();i++){
+
+                preparing += i+1+". " + preparingArray.getString(i) + "\n\n";
+            }
+            fullData = json.getString("title")+ ": \n\n" + json.getString("description")+ "\n\nIngredients: \n\n"
+                    + ingredients +"\n\nPreparing: \n\n"+ preparing+ "\n\nImages: \n\n";
+
+            JSONArray imgArray = json.getJSONArray("imgs");
+            img1 = imgArray.getString(0);
+            img2 = imgArray.getString(1);
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        SecondFragment.txtRecipe.setText(this.fullData);
+        Glide.with (SecondFragment.img2).load(img2).into(SecondFragment.img2);
+        Glide.with (SecondFragment.img1).load(img1).into(SecondFragment.img1);
+    }
+}
